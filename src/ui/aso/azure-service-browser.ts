1eeb06fdc3efdea42976a9f08a6c1f60c414d572
itowlson <ivan.towlson@microsoft.com>
2020-10-29 09:40:53 +1300

    Get real ASO API resources

--- src/ui/aso/azure-service-browser.ts
+++ src/ui/aso/azure-service-browser.ts
@@ -2,11 +2,12 @@
 import * as k8s from 'vscode-kubernetes-tools-api';
 
 export async function aso(explorer: k8s.ClusterExplorerV1): Promise<k8s.ClusterExplorerV1.NodeContributor> {
-    const pinnedKinds = await pinnedServiceKinds();
-    const resourceFolderChildren = pinnedKinds.map((k) => explorer.nodeSources.resourceFolder(k.displayName, k.displayName, k.kind, k.resourceType));
     const allKinds = await allServiceKinds();
-    const allFolderChildren = allKinds.map((k) => explorer.nodeSources.resourceFolder(k.displayName, k.displayName, k.kind, k.resourceType));
+    const allFolderChildren = allKinds.map((k) => explorer.nodeSources.resourceFolder(k.displayName, k.displayName, k.manifestKind, k.abbreviation));
     const allFolder = explorer.nodeSources.groupingFolder("All Service Types", undefined, ...allFolderChildren);
+
+    const pinnedKinds = await pinnedServiceKinds(allKinds);
+    const resourceFolderChildren = pinnedKinds.map((k) => explorer.nodeSources.resourceFolder(k.displayName, k.displayName, k.manifestKind, k.abbreviation));
 
     const children = [...resourceFolderChildren, allFolder];
     const servicesFolder = explorer.nodeSources.groupingFolder("Azure Services", undefined, ...children);
@@ -67,8 +68,8 @@ export async function aso(explorer: k8s.ClusterExplorerV1): Promise<k8s.ClusterE
 
 export interface AzureServiceKind {
     readonly displayName: string;
-    readonly kind: string;
-    readonly resourceType: string;
+    readonly manifestKind: string;
+    readonly abbreviation: string;
 }
 
 // interface AzureServiceInstance {
@@ -77,18 +78,41 @@ export interface AzureServiceKind {
 //     readonly resourceType: string;
 // }
 
-async function pinnedServiceKinds(): Promise<AzureServiceKind[]> {
-    return [
-        { displayName: "Event Hubs", kind: "Eventhub", resourceType: "eventhubs" },
-        { displayName: "MySQL Servers", kind: "MySQLServer", resourceType: "mysqlservers" },
-    ];
+async function pinnedServiceKinds(allKinds: AzureServiceKind[]): Promise<AzureServiceKind[]> {
+    const pinnedKindNames = ["mysqldatabases", "eventhubs"];  // TODO: get from config
+    return allKinds.filter((k) => pinnedKindNames.includes(k.abbreviation));
 }
 
+// should be errorable but skip for prototype
 async function allServiceKinds(): Promise<AzureServiceKind[]> {
-    return [
-        { displayName: "Blob Storage Accounts", kind: "BlobStorageAccount", resourceType: "blobstorageaccounts" },
-        { displayName: "Event Hubs", kind: "Eventhub", resourceType: "eventhubs" },
-        { displayName: "MySQL Servers", kind: "MySQLServer", resourceType: "mysqlservers" },
-        { displayName: "Service Buses", kind: "ServiceBus", resourceType: "servicebus" },
-    ];
+    const kubectl = await k8s.extension.kubectl.v1;
+    if (!kubectl.available) {
+        return [];  // TODO: ERROR
+    }
+    const sr = await kubectl.api.invokeCommand("api-resources --api-group azure.microsoft.com --no-headers");
+    if (!sr || sr.code !== 0) {
+        return [];  // TODO: ERROR
+    }
+
+    const lines = sr.stdout.split("\n").map((l) => l.trim()).filter((l) => l.length > 0);
+    return lines.map((l) => parseServiceResource(l));
+    // return [
+    //     { displayName: "Blob Storage Accounts", kind: "BlobStorageAccount", resourceType: "blobstorageaccounts" },
+    //     { displayName: "Event Hubs", kind: "Eventhub", resourceType: "eventhubs" },
+    //     { displayName: "MySQL Servers", kind: "MySQLServer", resourceType: "mysqlservers" },
+    //     { displayName: "Service Buses", kind: "ServiceBus", resourceType: "servicebus" },
+    // ];
+}
+
+function parseServiceResource(text: string): AzureServiceKind {
+    const bits = text.split(' ').filter((s) => s.length > 0);
+    if (bits.length === 4) {
+        // name, apigroup, namespaced, kind
+        return { displayName: bits[3], manifestKind: bits[3], abbreviation: bits[0] };
+    } else if (bits.length === 5) {
+        // name, shortnames, apigroup, namespaced, kind
+        return { displayName: bits[4], manifestKind: bits[4], abbreviation: bits[0] };
+    } else {
+        return { displayName: "WAT " + text, manifestKind: "WAT", abbreviation: "wat" };
+    }
 }
